d882a70c59571d13f3328f4b120b5a090c310dc7
XZhong <38326673+xzhong411@users.noreply.github.com>
2024-10-28 17:28:12 +0800

    Update run.sh

--- SFormer/run.sh
+++ SFormer/run.sh
@@ -1,8 +1,8 @@
 
 # ###### Generating CAMs ##########
 # CUDA_VISIBLE_DEVICES=0 python main.py  --data-set VOC12MS \
-#                 --img-list /data0/zhongxiang/MCT+/VOC2012/ImageSets/Segmentation \
-#                 --data-path /data0/zhongxiang/MCT+/VOC2012 \
+#                 --img-list Dataset/VOC2012/ImageSets/Segmentation \
+#                 --data-path Dataset/VOC2012 \
 #                 --gen_attention_maps \
 #                 --resume saved_model/checkpoint.pth \
 #                 --cam-npy-dir results/voc/cam-npy \
@@ -11,7 +11,7 @@
 # # # # ######### Evaluating the generated CAMs   ##########
 
 python evaluation.py --list voc12/train_id.txt \
-                     --gt_dir /data0/zhongxiang/MCT+/VOC2012/SegmentationClass \
+                     --gt_dir Dataset/VOC2012/SegmentationClass \
                      --logfile saved_model/cam.txt \
                      --type npy \
                      --curve True \
